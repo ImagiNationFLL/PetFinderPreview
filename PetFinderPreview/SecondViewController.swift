f69448e9ea0d978a9789804ff847dae1d20ccf65
Brice Dobry <brice@obycode.com>
2016-11-21 20:25:57 -0500

    Initial commit. Demo only

--- PetFinderPreview/SecondViewController.swift
+++ PetFinderPreview/SecondViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SecondViewController: UIViewController {
+    
+    // make sure to add this sound to your project
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,20 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clickTapped(_ sender: Any) {
+        let clickSound = Bundle.main.path(forResource: "click", ofType: "wav")
+            
+        var audioFileUrl = NSURL.fileURL(withPath: clickSound!)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: clickSound! ))
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print(error)
+        }
+        audioPlayer.play()
+        print("got in here")
+    }
 
 }
 
